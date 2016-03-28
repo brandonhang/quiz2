51f9f1ddfd039dfb194917b0024e0933d9756f25
Brandon Hang <brandonshang@gmail.com>
2016-03-28 19:57:51 -0400

    Squashed many a bug.  Added comic book title and cookie functionality.

--- template/quiz2.js
+++ template/quiz2.js
@@ -4,36 +4,90 @@
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
-	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	var $mouseover = $('.mouse-over');
+	var $click = $('.click');
+	var $submit = $('.submit');
 
 	$mouseover.on('mouseover', function() {
 		$this = $(this);
-		$(this).html('Scrooge McDuck!');
+		$(this).html('<p>Scrooge McDuck!</p>');
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
+	$click.on('click', function() {
+		$this.html('<p>Peace Out!</p>');
 		$(this).fadeout(1500);
-		return false;
 	});
 
-	$submit.on('submit', function(e) {
+	$('form').submit('submit', function(e) {
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+			$(this).find('input[type="text"]').each(function() {
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append('<h2>Congratulations! You\'ve entered some text!</h2>');
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
+	$(document).ready(function() {
+		setTimeout(function() {
+			$('.timeout').fadeIn('slow');
 		}, 1000);
 	});
 
 })(jQuery);
+
+$(document).ready(function() {
+	var randyNum;
+	var titleArr = [];
+	var bakedGood = document.cookie;
+	
+	if (bakedGood.includes("quiz2")) {
+		var afterSign = bakedGood.indexOf('=') + 1;
+		randyNum = Number(bakedGood.substring(afterSign, bakedGood.length));
+		$.ajax({
+			url: 'http://www.mattbowytz.com/simple_api.json',
+			type: 'GET',
+			datatype: 'json',
+			data: { data: 'quizData' },
+			success: function(data) {
+				titleArr = data.data;
+				$('#button1').html("Change It");
+				var randyTitle = titleArr[randyNum];
+				$('#data').text(randyTitle);
+				$('.api').append("<button class='button' id='button2'>Keep It</button>");
+			}
+		});
+	}
+	
+	$('#button1').on('click', function() {
+		if (titleArr.length == 0) {
+			$.ajax({
+				url: 'http://www.mattbowytz.com/simple_api.json',
+				type: 'GET',
+				datatype: 'json',
+				data: { data: 'quizData' },
+				success: function(data) {
+					titleArr = data.data;
+					$('#button1').html("Change It");
+					randyNum = Math.floor(Math.random() * titleArr.length);
+					var randyTitle = titleArr[randyNum];
+					$('#data').text(randyTitle);
+					$('.api').append("<button class='button' id='button2'>Keep It</button>");
+				}
+			});
+		}
+		else {
+			randyNum = Math.floor(Math.random() * titleArr.length);
+			var randyTitle = titleArr[randyNum];
+			$('#data').text(randyTitle);
+		}
+	});
+	
+	$(document).on('click', '#button2', function() {
+		document.cookie="quiz2=" + randyNum +"; expires=(Date.getTime() * 1000 + 60)";
+		console.log(randyNum);
+	});
+	
+	
+});
